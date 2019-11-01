831635095ac7f8285c61884505cb06601348f9bb
Mikalai Biadrytski <Mikalai_Biadrytski@epam.com>
2019-11-01 14:07:28 +0300

    Refactoring

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField
+)
+from wtforms.validators import (
+    ValidationError,
+    DataRequired,
+    Email,
+    EqualTo
+)
 from app.models import User
 
 
